791642f906c2b5d19bc6f12fed171935fe2b10d6
liujianhua <1769908045@qq.com>
2018-07-10 17:23:49 +0800

    router-forEach

--- src/router/router-control.js
+++ src/router/router-control.js
@@ -3,6 +3,9 @@ import syncState from '../unitils/sync-state'
 export default (store, router) => {
   router.beforeEach((to, from, next) => {
     syncState(store)
+    if (to.matched.some(record => record.meta.title && typeof document !== 'undefined')) {
+      document.title = to.meta.title
+    }
     if (to.matched.some(record => record.meta.requiresAuth)) {
       if (store.state.token) {
         next()
@@ -14,10 +17,6 @@ export default (store, router) => {
         })
       }
     } else {
-      next()
-    }
-    if (to.matched.some(record => record.meta.title)) {
-      typeof document !== 'undefined' && (document.title = to.meta.title)
       next()
     }
   })
