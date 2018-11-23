0d554ed57de3d20093aa7631a13cf68ea3dd1d88
liujianhua <1769908045@qq.com>
2018-11-23 22:01:15 +0800

    fix：child components execute asyncData and add explanatory

--- src/entry-client.js
+++ src/entry-client.js
@@ -8,24 +8,39 @@ document.body.appendChild(processBar.$el)
 const { app, router, store } = createApp()
 router.onReady(() => {
   router.beforeResolve((to, from, next) => {
+    // 若访问的是子路由getMatchedComponents结果length>1
     const matched = router.getMatchedComponents(to)
     const prevMatched = router.getMatchedComponents(from)
     let diffed = false
+    // 多级路由从第一个不同路由组件开始渲染，避免重复渲染相同的父组件
     const activated = matched.filter((c, i) => {
       return diffed || (diffed = (prevMatched[i] !== c))
     })
+    // 访问的是同一个路由直接next跳出
     if (!activated.length) {
       return next()
     }
     processBar.start()
-    Promise.all(activated.map(c => {
-      if (c.asyncData) {
-        return c.asyncData({ store, route: to })
+
+    // 将asyncData钩子加入到组件生命周期，包括子组件
+    let asyncDataTask = []
+    const searchSyncData = component => {
+      component.asyncData && asyncDataTask.push(component.asyncData({ store, route: to }))
+      // 递归遍历子组件
+      if (component.components) {
+        let components = component.components
+        for (let i in components) {
+          searchSyncData(components[i])
+        }
       }
-    })).then(() => {
+    }
+    activated.forEach(i => searchSyncData(i))
+
+    Promise.all(asyncDataTask).then(() => {
       processBar.end()
       next()
     }).catch(next)
   })
+
   app.$mount('#app')
 })
--- src/entry-server.js
+++ src/entry-server.js
@@ -3,29 +3,28 @@ import { createApp } from './main'
 export default context => {
   const { app, router, store } = createApp()
   store.commit('setToken', context.token)
+
   return new Promise((resolve, reject) => {
     router.push(context.url)
     router.onReady(() => {
       const matchedComponents = router.getMatchedComponents()
       !matchedComponents.length && reject(404)
+
       // 将asyncData钩子加入到组件生命周期，包括子组件
-      let asyncTask = []
-      const searchSyncData = target => {
-        if (target.asyncData) {
-          asyncTask.push(target.asyncData({ store, route: router.currentRoute }))
-        }
+      let asyncDataTask = []
+      const searchSyncData = component => {
+        component.asyncData && asyncDataTask.push(component.asyncData({ store, route: router.currentRoute }))
         // 递归遍历子组件
-        if (target.components) {
-          let components = target.components
+        if (component.components) {
+          let components = component.components
           for (let i in components) {
-            if (components.hasOwnProperty(i)) {
-              searchSyncData(components[i])
-            }
+            searchSyncData(components[i])
           }
         }
       }
       matchedComponents.forEach(i => searchSyncData(i))
-      Promise.all(asyncTask).then(() => {
+
+      Promise.all(asyncDataTask).then(() => {
         context.state = store.state
         resolve(app)
       }).catch(reject)
