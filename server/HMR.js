e4e31a91290625834b541133fca449a3d0272edd
liujianhua <1769908045@qq.com>
2019-01-17 22:54:32 +0800

    perf：删除koa-webpack-middleware-zm

--- server/HMR.js
+++ server/HMR.js
@@ -3,7 +3,8 @@ const { join } = require('path')
 const MFS = require('memory-fs')
 const webpack = require('webpack')
 const chokidar = require('chokidar')
-const { koaDevMiddleware, koaHotMiddleware } = require('koa-webpack-middleware-zm')
+const webpackDev = require('webpack-dev-middleware')
+const webpackHot = require('webpack-hot-middleware')
 const clientConfig = require('../build/client')
 const serverConfig = require('../build/server')
 
@@ -29,10 +30,11 @@ module.exports = function setupDevServer(app, templatePath, createRender) {
   })
 
   const clientCompiler = webpack(clientConfig)
-  const devMiddleware = require('webpack-dev-middleware')(clientCompiler, {
+  const devMiddleware = webpackDev(clientCompiler, {
     publicPath: clientConfig.output.publicPath,
     noInfo: true
   })
+
   clientCompiler.plugin('done', stats => {
     stats = stats.toJson()
     stats.errors.forEach(err => console.error(err))
@@ -42,10 +44,29 @@ module.exports = function setupDevServer(app, templatePath, createRender) {
     update()
   })
 
+  const koaDevMiddleware = devMiddleware => {
+    return (ctx, next) => {
+      return new Promise((resolve) => {
+        const option = {
+          end: content => {
+            ctx.body = content
+            resolve(false)
+          },
+          setHeader: (name, value) => ctx.set(name, value)
+        }
+        devMiddleware(ctx.req, option, () => resolve(true))
+      }).then(err => err ? next() : null)
+    }
+  }
+
+  const koaHotMiddleware = hotMiddleware => (ctx, next) => {
+    return new Promise(resolve => {
+      hotMiddleware(ctx.req, ctx.res, resolve)
+    }).then(next)
+  }
+
   app.use(koaDevMiddleware(devMiddleware))
-  app.use(koaHotMiddleware(require('webpack-hot-middleware')(clientCompiler,
-    { heartbeat: 5000 }
-  )))
+  app.use(koaHotMiddleware(webpackHot(clientCompiler, { heartbeat: 5000 })))
 
   const serverCompiler = webpack(serverConfig)
   const mfs = new MFS()
